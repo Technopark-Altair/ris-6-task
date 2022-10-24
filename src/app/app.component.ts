8408f58f2ca11a8d74a84d03f39ad49763b7e29e
0ReC0 <kitanin.ser@mail.ru>
2022-10-24 16:19:22 +0300

    feat: add binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-project';
+  name = 'qweweWz`';
+  text = 'asda'
+  price = 100
+  itemAdded = ''
+  counter = 0
+
+  addItem(text: string, price: number) {
+    this.itemAdded = text + ` ${price} $ ${this.counter}`
+    this.counter = this.counter + 1
+  }
 }
